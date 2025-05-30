fe8142d279e4fde70aa72b60b83f260e212b7b70
fikarme <fikarme@pm.me>
2025-05-30 16:59:07 +0300

    kys

---
+++ ex01/main.cpp
@@ -0,0 +1,24 @@
+#include "ScalarConverter.hpp"
+
+int main() {
+
+    int x = 5;
+    int y = 2;
+    double z;
+
+    z = static_cast<double>(x) / y;
+    cout << "static_cast<double>(x) / y -> " << z << endl;
+
+    z = x / static_cast<double>(y);
+    cout << "x / static_cast<double>(y) -> " << z << endl;
+
+    z = static_cast<double>(x) / static_cast<double>(y);
+    cout << "static_cast<double>(x) / static_cast<double>(y) -> " << z << endl;
+
+    z = x / y;
+    cout << "x / y -> " << z << endl;
+
+    z = static_cast<double>(x / y);
+    cout << "static_cast<double>(x / y) -> " << z << endl;
+
+}
